8be8a8fc9bddc512d27c6b18e6682a3ac6f04d95
Mark Nosal <mark.c.nosal@gmail.com>
2023-10-04 07:11:17 -0500

    add seed for requests table

--- server/seed.py
+++ server/seed.py
@@ -24,13 +24,15 @@ if __name__ == '__main__':
         print('Rows cleared...')
 
         # add user
+        users = []
         new_user = User(
             name=fake.name(),
             age=fake.random_int(min=18, max=99),
             email=fake.email(),
             earnings=0.00,
         )
-        db.session.add(new_user)
+        users.append(new_user)
+        db.session.add_all(users)
         db.session.commit()
         print('User created...')
 
@@ -51,14 +53,35 @@ if __name__ == '__main__':
         pets = []
         available_species = ['dog', 'cat', 'bird', 'turtle', 'mammal', 'reptile', 'amphibian']
         for i in range(30):
-            random_client = rc(clients)
             new_pet = Pet(
                 name=fake.first_name(),
                 age=fake.random_int(min=0, max=200),
                 species=rc(available_species),
-                client_id=random_client.id
+                client_id=rc(clients).id
             )
             pets.append(new_pet)
         db.session.add_all(pets)
         db.session.commit()
         print('Pets created...')
+
+        # add requests
+        requests = []
+        for i in range(15):
+            rand_client = rc(clients)
+            rand_client_pets = [pet.name for pet in rand_client.pets]
+            if rand_client_pets:
+                details = f'{rc(rand_client_pets)}, ' + fake.sentence()
+            else:
+                details = fake.sentence()
+            new_request = Request(
+                details=details,
+                location=fake.address(),
+                price=randint(20, 500),
+                client_id=rand_client.id,
+                user_id=rc(users).id,
+            )
+            requests.append(new_request)
+        db.session.add_all(requests)
+        db.session.commit()
+        print('Requests created...')
+
