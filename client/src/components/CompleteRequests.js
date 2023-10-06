76c1f9aa7b48d98f89b20a93b21aa3d70e66e181
Mark Nosal <mark.c.nosal@gmail.com>
2023-10-06 13:50:16 -0500

    flesh out CompleteRequests component

--- client/src/components/CompleteRequests.js
+++ client/src/components/CompleteRequests.js
@@ -3,8 +3,15 @@ import RequestCard from './RequestCard';
 
 export default function CompleteRequests({ allRequests }) {
     return (
-        <div className="completeRequests">
-            completeRequests
+        <div className="completeRequests-container">
+            <h2>Completed Requests</h2>
+            <div className="completeRequests-card-container">
+                {allRequests.map(r => (
+                    <div key={r.id} className="completeRequests-card">
+                        <RequestCard request={r} />
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
