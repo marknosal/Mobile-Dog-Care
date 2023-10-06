3a68f87412f044ac18dc1cae1f35c33a4db7e9cf
Mark Nosal <mark.c.nosal@gmail.com>
2023-10-06 08:34:58 -0500

    add request state and useEffect to fetch all requests on page load

--- client/src/components/Requests.js
+++ client/src/components/Requests.js
@@ -1,15 +1,22 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 export default function Requests() {
+    const [requests, setRequests] = useState([])
+    useEffect(() => {
+        fetch('/requests')
+            .then(response => response.json())
+            .then(data => setRequests(data))
+    }, [])
     return (
         <div className="requests-main">
             <div className='requests-incomplete'>
                 incomplete
+                <hr/>
             </div>
             <div className='requests-complete'>
                 complete
+                <hr/>
             </div>
-            Requests
         </div>
     )
 }
