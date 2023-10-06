a88f67a0a8948babe4ca94ef7545107b5ba43d92
Mark Nosal <mark.c.nosal@gmail.com>
2023-10-06 08:24:24 -0500

    change html title to Mobile Dog Care

--- client/src/components/Requests.js
+++ client/src/components/Requests.js
@@ -2,14 +2,14 @@ import React from "react"
 
 export default function Requests() {
     return (
-        // <div className="requests-main">
-        //     <div className='requests-incomplete'>
-        //         incomplete
-        //     </div>
-        //     <div className='requests-complete'>
-        //         complete
-        //     </div>
-        //     Requests
-        // </div>
+        <div className="requests-main">
+            <div className='requests-incomplete'>
+                incomplete
+            </div>
+            <div className='requests-complete'>
+                complete
+            </div>
+            Requests
+        </div>
     )
 }
