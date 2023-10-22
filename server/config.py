e1e2a5544bf87ec07de4bfdaece86b460c681dbb
Mark Nosal <mark.c.nosal@gmail.com>
2023-10-21 20:03:41 -0500

    add bcrypt

--- server/config.py
+++ server/config.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 from flask_migrate import Migrate
 from flask_restful import Api
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
@@ -28,6 +29,7 @@ metadata = MetaData(naming_convention=naming_convention)
 db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
 db.init_app(app)
+bcrypt = Bcrypt(app)
 
 # Instantiate REST API
 api = Api(app)
